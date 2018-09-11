02c4023cf1ed31c2b7bab72366667017e69512f6
Alexey Kopytko <alexey@kopytko.com>
2018-09-11 18:35:24 +0900

    Deserialization/DeliveryResponseTest for a failed request

---
+++ tests/Deserialization/DeliveryResponseTest.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of Cdek SDK package.
+ *
+ * © Appwilio (http://appwilio.com), greabock (https://github.com/greabock), JhaoDa (https://github.com/jhaoda)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Appwilio\CdekSDK\Deserialization;
+
+use Appwilio\CdekSDK\Responses\DeliveryResponse;
+use Tests\Appwilio\CdekSDK\Fixtures\FixtureLoader;
+
+/**
+ * @covers \Appwilio\CdekSDK\Responses\DeliveryResponse
+ * @covers \Appwilio\CdekSDK\Common\DeliveryRequest
+ */
+class DeliveryResponseTest extends TestCase
+{
+    public function test_failing_request()
+    {
+        $response = $this->getSerializer()->deserialize(FixtureLoader::load('DeliveryRequestFailed.xml'), DeliveryResponse::class, 'xml');
+
+        /** @var $response DeliveryResponse */
+        $this->assertInstanceOf(DeliveryResponse::class, $response);
+        $this->assertStringStartsWith('ERR_', $response->getRequests()[0]->getErrorCode());
+    }
+}

654ea9d741273997c3a74e021d153343d04f08c5
Alexey Kopytko <alexey@kopytko.com>
2018-09-13 18:20:03 +0900

    DeleteRequestTest

---
+++ tests/Serialization/DeleteRequestTest.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This code is licensed under the MIT License.
+ *
+ * Copyright (c) 2018 appwilio <appwilio.com>
+ * Copyright (c) 2018 JhaoDa <jhaoda@gmail.com>
+ * Copyright (c) 2018 greabock <greabock17@gmail.com>
+ * Copyright (c) 2018 Alexey Kopytko <alexey@kopytko.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\CdekSDK\Serialization;
+
+use CdekSDK\Common\Order;
+use CdekSDK\Requests\DeleteRequest;
+
+/**
+ * @covers \CdekSDK\Requests\DeleteRequest
+ */
+class DeleteRequestTest extends TestCase
+{
+    public function test_can_serialize()
+    {
+        $result = $this->getSerializer()->serialize(DeleteRequest::create([
+            'Number' => 'foo',
+        ])->addOrder(new Order([
+            'Number' => 'bar',
+        ])), DeleteRequest::SERIALIZATION_XML);
+
+        $this->assertSame('<?xml version="1.0" encoding="UTF-8"?>
+<DeleteRequest OrderCount="1" Number="foo">
+  <Order Number="bar"/>
+</DeleteRequest>
+', $result);
+    }
+}

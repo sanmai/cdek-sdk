429ea84e777172f2d8e3859a897aaf78c962b3c0
Alexey Kopytko <alexey@kopytko.com>
2018-09-17 11:51:57 +0900

    StatusReportResponseTest for a minimal real-life response

--- tests/Deserialization/StatusReportResponseTest.php
+++ tests/Deserialization/StatusReportResponseTest.php
@@ -28,6 +28,7 @@ declare(strict_types=1);
 
 namespace Tests\CdekSDK\Deserialization;
 
+use CdekSDK\Common\Order;
 use CdekSDK\Common\Reason;
 use CdekSDK\Common\State;
 use CdekSDK\Common\Status;
@@ -97,5 +98,38 @@ class StatusReportResponseTest extends TestCase
         $this->assertSame('Вручен', $lastState->Description);
         $this->assertSame('Нальчик', $lastState->CityName);
         $this->assertSame(1081, $lastState->CityCode);
+    }
+
+    public function test_it_reads_simple_response()
+    {
+        $response = $this->getSerializer()->deserialize(FixtureLoader::load('StatusReportMinimal.xml'), StatusReportResponse::class, 'xml');
+
+        /** @var $response StatusReportResponse */
+        $this->assertInstanceOf(StatusReportResponse::class, $response);
+
+        $this->assertSame(strtotime('2000-12-31T17:00:00+00:00'), $response->getDateFirst()->getTimestamp());
+        $this->assertSame(strtotime('2018-09-01T00:00:00+00:00'), $response->getDateLast()->getTimestamp());
+
+        $this->assertCount(1, $response->getOrders());
+
+        /** @var Order $order */
+        $order = $response->getOrders()[0];
+
+        $this->assertInstanceOf(Order::class, $order);
+        $this->assertSame('TESTING123', $order->ActNumber);
+        $this->assertSame('TEST-123456', $order->getNumber());
+        $this->assertSame('1234567', $order->getDispatchNumber());
+        $this->assertNull($order->DeliveryDate);
+        $this->assertSame('', $order->getRecipientName());
+
+        $this->assertSame(strtotime('2018-09-01T02:10:00+00:00'), $order->getStatus()->getDate()->getTimestamp());
+        $this->assertSame(1, $order->getStatus()->getCode());
+
+        $this->assertSame('Создан', $order->getStatus()->getDescription());
+        $this->assertSame(44, $order->getStatus()->getCityCode());
+        $this->assertSame('Москва', $order->getStatus()->getCityName());
+
+        $this->assertSame(0, $order->getReason()->Code);
+        $this->assertSame(0, $order->getDelayReason()->Code);
     }
 }

57f5e12c6570e27631255dca9e7530430ab17caa
Alexey Kopytko <alexey@kopytko.com>
2018-09-25 12:43:16 +0900

    CallDelay: deprecate property access

--- src/Common/CallDelay.php
+++ src/Common/CallDelay.php
@@ -43,6 +43,8 @@ final class CallDelay
      * @JMS\Type("DateTimeImmutable<'Y-m-d\TH:i:sP'>")
      *
      * @var \DateTimeImmutable
+     *
+     * @deprecated
      */
     public $Date;
 
@@ -52,14 +54,22 @@ final class CallDelay
      * @JMS\Type("DateTimeImmutable<'Y-m-d\TH:i:sP'>")
      *
      * @var \DateTimeImmutable
+     *
+     * @deprecated
      */
     public $DateNext;
 
+    /**
+     * @phan-suppress PhanDeprecatedProperty
+     */
     public function getDate(): \DateTimeImmutable
     {
         return $this->Date;
     }
 
+    /**
+     * @phan-suppress PhanDeprecatedProperty
+     */
     public function getDateNext(): \DateTimeImmutable
     {
         return $this->DateNext;
